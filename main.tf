ee2f47aebb238ecef74e0d13d687692e67e61a61
Andrew Parks <aparks@gmail.com>
2023-10-06 17:32:37 -0400

    23 configuration drift (#24)
    * #23 bucket should no longer use random provider
    
    * #23 fix tfvars example

--- main.tf
+++ main.tf
@@ -1,20 +1,13 @@
-# https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
-resource "random_string" "bucket_name" {
-  length    = 16
-  special   = false
-  upper     = false
-  # lower=true by default
-}
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
-resource "aws_s3_bucket" "example" {
+resource "aws_s3_bucket" "website_bucket" {
   # Bucket Naming Rules
   # https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
-  bucket = random_string.bucket_name.result
+  bucket = var.bucket_name
 
-  # tags = {
-  #   UserUuid = var.user_uuid
-  # }
+  tags = {
+    UserUuid = var.user_uuid
+  }
 
 }
 
