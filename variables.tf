4eeb7d708f6a36aa582aa48c1ed7b306793597e5
Andrew Parks <aparks@gmail.com>
2023-10-14 01:06:34 -0400

    #46 tested my terra home on missingo (#47)

--- variables.tf
+++ variables.tf
@@ -1,10 +1,18 @@
-variable "user_uuid" {
-  type = string
+variable "terratowns_endpoint" {
+ type = string
 }
 
-variable "bucket_name" {
-  type = string
+variable "terratowns_access_token" {
+ type = string
 }
+
+variable "teacherseat_user_uuid" {
+ type = string
+}
+
+# variable "bucket_name" {
+#   type = string
+# }
 
 variable "index_html_filepath" {
   type = string
@@ -21,3 +29,4 @@ variable "content_version" {
 variable "assets_path" {
   type = string
 }
+
